f3567628f1fa3163177efa264cc8f2beddb6d0b0
Krishna2328 <33080377+Krishna2328@users.noreply.github.com>
2021-02-08 13:02:27 +0530

    app folder added

---
+++ app/src/main/java/com/krishna/estoretemp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krishna.estoretemp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
